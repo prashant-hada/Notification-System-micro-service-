d7dd887f4f6fbff77855b8d925ad3a28e0246419
Prashant <hadaprashant2002@gmail.com>
2024-11-10 01:18:30 +0530

    created Template routes

---
+++ routes/templateRoute.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+    getTemplateById, 
+    getAllTemplates, 
+    createTemplate, 
+    updateTemplate, 
+    deleteTemplate
+} from "../controllers/templateController"
+
+const router = Router();
+
+router.get("get-one/:templateId", getTemplateById);
+router.get("/get-all",getAllTemplates);
+router.post("/create", createTemplate);
+router.patch("/update:templateId", updateTemplate);
+router.delete("/delete/:templateId", deleteTemplate)
+
+export default router;
