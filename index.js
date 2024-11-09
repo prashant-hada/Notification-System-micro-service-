b4c7f21232c565642798dcd4f68c87a6abef33c7
Prashant <hadaprashant2002@gmail.com>
2024-11-10 01:18:30 +0530

    created Template routes

--- index.js
+++ index.js
@@ -13,21 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/",AppRouter);
-app.get("/email-send",async(req,res,next)=>{
-    try {
-        const {
-            to="hadaprashant2002@gmail.com", 
-            subject="Test mail Hihi hoohoo", 
-            message = "He he my boiii how's it goin baby, la la la"
-        }=req.query;
-
-        const info = await sendEmail(to,subject,message);
-
-        return res.status(200).json({message:"API to chali hai Boss", info});
-    } catch (error) {
-       return res.status(400).json({message:"some error occured", error}) 
-    }
-})
 
 app.listen(PORT,()=>{
     console.log(`Service is running on PORT ${PORT}`)
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import DummyDataRouter from "./dummyDataRoute.js"
+import TemplateRouter from "./templateRoute.js"
 
 const router = Router();
 router.get("/",(req,res,next)=>{
     res.send("Hi from server")
 })
+
 router.use("/create-data",DummyDataRouter);
+router.use("template",TemplateRouter)
 
 export default router;
