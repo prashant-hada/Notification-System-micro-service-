5dc242618d8bc6e9f0999384aafd81828c34f10b
Prashant <hadaprashant2002@gmail.com>
2024-11-11 00:35:28 +0530

    Pm2 Configured

--- utils/jobQueue.js
+++ utils/jobQueue.js
@@ -2,7 +2,7 @@ import {Queue} from "bullmq";
 import Redis from "ioredis"
 
 const redisConnection = new Redis({
-  host: '1localhost', // Replace with your Redis host if needed
+  host: 'localhost', // Replace with your Redis host if needed
   port: 6379,        // Replace with your Redis port if needed
 });
 
