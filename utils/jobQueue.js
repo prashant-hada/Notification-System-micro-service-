a9b2820a7efaea670f99874ea9b241f1af13617a
Prashant <hadaprashant2002@gmail.com>
2024-11-11 00:02:33 +0530

    added queue and worker functionality

---
+++ utils/jobQueue.js
@@ -0,0 +1,13 @@
+import {Queue} from "bullmq";
+import Redis from "ioredis"
+
+const redisConnection = new Redis({
+  host: '1localhost', // Replace with your Redis host if needed
+  port: 6379,        // Replace with your Redis port if needed
+});
+
+const notificationQueue = new Queue('notifications', {
+  connection: redisConnection
+});
+
+export {notificationQueue, redisConnection};
